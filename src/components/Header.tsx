8662f10ffbbdd129deab32d73a4f93d5b751f6cf
happer64bit <happer64bit@gmail.com>
2024-09-03 21:56:42 +0630

    set align item center for header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <header class="bg-[#f6f8fa] border-b border-b-[#d1d9e0] dark:bg-[#010409] dark:border-b-[#3d444d] dark:text-white w-full">
-            <nav class="px-4 py-4 mx-auto max-w-[1400px] flex justify-between">
+            <nav class="px-4 py-4 mx-auto max-w-[1400px] flex justify-between items-center">
                 <a href="/" class="font-bold">
                     Issue Template Generator
                 </a>
